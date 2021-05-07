a08cbcf89c99d5f3f2c5e49a437a643683b2fcfa
Stanislav Nanoshkin <nanstas@yandex.ru>
2021-05-07 20:59:52 +0500

    replaced snake case with camel case

--- app/src/main/java/ru/netology/fmhandroid/ui/MainActivity.kt
+++ app/src/main/java/ru/netology/fmhandroid/ui/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ru.netology.fmhandroid.R
 
-class main_activity : AppCompatActivity(R.layout.patient_profile_card) {
+class MainActivity : AppCompatActivity(R.layout.patient_profile_card) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.patient_profile_card)
