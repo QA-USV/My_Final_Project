452e77164b0e24e06a04cb08ec50eefee524c8f6
Stanislav Nanoshkin <nanstas@yandex.ru>
2021-05-29 10:52:53 +0500

    deleted unnecessary code

--- app/src/main/java/ru/netology/fmhandroid/ui/MainActivity.kt
+++ app/src/main/java/ru/netology/fmhandroid/ui/MainActivity.kt
@@ -4,9 +4,4 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ru.netology.fmhandroid.R
 
-class MainActivity : AppCompatActivity(R.layout.fragment_patient_profile_card) {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_patient_profile_card)
-    }
-}
+class MainActivity : AppCompatActivity(R.layout.fragment_patient_profile_card)
