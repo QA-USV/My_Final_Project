55f95d780cee74b2d500ec3d2805354dab26635f
Dolgoff62 <dolgov2007@inbox.ru>
2021-06-01 20:56:44 +0300

    Delete MainActivity.kt

--- app/src/main/java/ru/netology/fmhandroid/ui/MainActivity.kt
+++
@@ -1,7 +0,0 @@
-package ru.netology.fmhandroid.ui
-
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import ru.netology.fmhandroid.R
-
-class MainActivity : AppCompatActivity(R.layout.fragment_patient_profile_card)
