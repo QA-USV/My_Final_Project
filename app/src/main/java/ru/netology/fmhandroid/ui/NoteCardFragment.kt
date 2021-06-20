8cf31bb1333ccf672766942b188d349071c67ec3
Stanislav Nanoshkin <nanstas@yandex.ru>
2021-06-20 19:47:10 +0500

    Update NoteCardFragment.kt

--- app/src/main/java/ru/netology/fmhandroid/ui/NoteCardFragment.kt
+++ app/src/main/java/ru/netology/fmhandroid/ui/NoteCardFragment.kt
@@ -1,25 +1,6 @@
 package ru.netology.fmhandroid.ui
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import ru.netology.fmhandroid.databinding.FragmentNoteCardBinding
+import ru.netology.fmhandroid.R
 
-class NoteCardFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-
-        val binding = FragmentNoteCardBinding.inflate(
-            inflater,
-            container,
-            false
-        )
-
-        return binding.root
-    }
-}
+class NoteCardFragment : Fragment(R.layout.fragment_note_card)
