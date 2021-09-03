2b1adbd6179a5c12e848c2348ee01d50319f8e40
Dolgoff62 <dolgov2007@inbox.ru>
2021-09-03 15:29:33 +0300

    Create claim comment entity

---
+++ app/src/main/java/ru/netology/fmhandroid/entity/ClaimCommentEntity.kt
@@ -0,0 +1,41 @@
+package ru.netology.fmhandroid.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import ru.netology.fmhandroid.dto.ClaimComment
+import java.time.LocalDateTime
+
+@Entity(tableName = "ClaimCommentEntity")
+data class ClaimCommentEntity(
+    @PrimaryKey
+    @ColumnInfo(name = "id")
+    val id: Int,
+    @ColumnInfo(name = "claimId")
+    val claimId: Int,
+    @ColumnInfo(name = "description")
+    val description: String,
+    @ColumnInfo(name = "creatorId")
+    val creatorId: Int,
+    @ColumnInfo(name = "createDate")
+    val createDate: LocalDateTime
+) {
+    fun toDto() = ClaimComment(
+        id = id,
+        claimId = claimId,
+        description = description,
+        creatorId = creatorId,
+        createDate = createDate
+    )
+}
+
+fun List<ClaimCommentEntity>.toDto(): List<ClaimComment> = map(ClaimCommentEntity::toDto)
+fun List<ClaimComment>.toEntity(): List<ClaimCommentEntity> = map(ClaimComment::toEntity)
+fun ClaimComment.toEntity() = ClaimCommentEntity(
+    id = id,
+    claimId = claimId,
+    description = description,
+    creatorId = creatorId,
+    createDate = createDate
+)
+
