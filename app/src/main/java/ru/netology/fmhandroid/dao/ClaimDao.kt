03f53329a8740bb6ad234c1493b15afca0554b75
Dmitriy Dolgov <dolgov2007@inbox.ru>
2021-09-06 17:43:59 +0300

    Create Claim Dao

---
+++ app/src/main/java/ru/netology/fmhandroid/dao/ClaimDao.kt
@@ -0,0 +1,29 @@
+package ru.netology.fmhandroid.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+import ru.netology.fmhandroid.entity.ClaimEntity
+
+@Dao
+interface ClaimDao {
+    @Query("SELECT * FROM ClaimEntity ORDER BY id DESC")
+    fun getAllClaims(): Flow<List<ClaimEntity>>
+
+    @Query("SELECT * FROM ClaimEntity WHERE status OPEN AND IN_PROGRESS")
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertClaim(claim: ClaimEntity)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertClaim(claims: List<ClaimEntity>)
+
+    @Query("SELECT * FROM ClaimEntity WHERE id = :id")
+    suspend fun getClaimById(id: Int): ClaimEntity
+
+    @Query("UPDATE ClaimEntity Set deleted = 1 WHERE id = :id")
+    suspend fun deleteClaimById(id: Int)
+
+}
