0b352838a2a3ac531caf331a7e9254f2b4657335
Dmitriy Dolgov <dolgov2007@inbox.ru>
2021-09-09 09:22:29 +0300

    Update ClaimListAdapter.kt

--- app/src/main/java/ru/netology/fmhandroid/adapter/ClaimListAdapter.kt
+++ app/src/main/java/ru/netology/fmhandroid/adapter/ClaimListAdapter.kt
@@ -10,63 +10,77 @@ import ru.netology.fmhandroid.dto.Claim
 import ru.netology.fmhandroid.utils.Utils
 
 interface OnClaimItemClickListener {
-    fun onCard(claim: Claim) {}
+    fun onCard(claimWithCreatorAndExecutor: Claim.ClaimWithCreatorAndExecutor) {}
 }
 
-class ClaimListAdapter (
+class ClaimListAdapter(
     private val onClaimItemClickListener: OnClaimItemClickListener
-    ) : ListAdapter<Claim, ClaimListAdapter.NoteViewHolder>(NoteDiffCallback()) {
+) : ListAdapter<Claim.ClaimWithCreatorAndExecutor, ClaimListAdapter.ClaimViewHolder>(
+    ClaimDiffCallback()
+) {
 
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
-            val binding = ItemClaimBinding.inflate(
-                LayoutInflater.from(parent.context),
-                parent,
-                false
-            )
-            return NoteViewHolder(binding, onClaimItemClickListener)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ClaimViewHolder {
+        val binding = ItemClaimBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return ClaimViewHolder(binding, onClaimItemClickListener)
+    }
+
+    override fun onBindViewHolder(holder: ClaimViewHolder, position: Int) {
+        getItem(position)?.let {
+            holder.bind(it)
         }
+    }
 
-        override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
-            getItem(position)?.let {
-                holder.bind(it)
-            }
-        }
+    class ClaimViewHolder(
+        private val binding: ItemClaimBinding,
+        private val onClaimItemClickListener: OnClaimItemClickListener
+    ) : RecyclerView.ViewHolder(binding.root) {
 
-        class NoteViewHolder(
-            private val binding: ItemClaimBinding,
-            private val onClaimItemClickListener: OnClaimItemClickListener
-        ) : RecyclerView.ViewHolder(binding.root) {
-
-            fun bind(claim: Claim) {
-                binding.apply {
-                    // executorNameMaterialTextView.text = TODO("Додумать как здесь получить исполнителя")
-                    planTimeMaterialTextView.text = claim.planExecuteDate?.let {
+        fun bind(claimWithCreatorAndExecutor: Claim.ClaimWithCreatorAndExecutor) {
+            binding.apply {
+                executorNameMaterialTextView.text = Utils.shortUserNameGenerator(
+                    claimWithCreatorAndExecutor.executor.firstName.toString(),
+                    claimWithCreatorAndExecutor.executor.lastName.toString(),
+                    claimWithCreatorAndExecutor.executor.middleName.toString()
+                )
+                planTimeMaterialTextView.text = claimWithCreatorAndExecutor
+                    .claim.planExecuteDate?.let {
                         Utils.convertTime(
                             it
                         )
                     }
-                    planDateMaterialTextView.text = claim.planExecuteDate?.let {
+                planDateMaterialTextView.text = claimWithCreatorAndExecutor
+                    .claim.planExecuteDate?.let {
                         Utils.convertDate(
                             it
                         )
                     }
-                    descriptionMaterialTextView.text = claim.description
+                descriptionMaterialTextView.text = claimWithCreatorAndExecutor.claim.description
 
-                    claimListCard.setOnClickListener {
-                        onClaimItemClickListener.onCard(claim)
-                    }
+                claimListCard.setOnClickListener {
+                    onClaimItemClickListener.onCard(claimWithCreatorAndExecutor)
                 }
             }
         }
+    }
 
-        class NoteDiffCallback : DiffUtil.ItemCallback<Claim>() {
-            override fun areItemsTheSame(oldItem: Claim, newItem: Claim): Boolean {
-                return oldItem == newItem
-            }
-
-            override fun areContentsTheSame(oldItem: Claim, newItem: Claim): Boolean {
-                return oldItem == newItem
-            }
+    class ClaimDiffCallback : DiffUtil.ItemCallback<Claim.ClaimWithCreatorAndExecutor>() {
+        override fun areItemsTheSame(
+            oldItem: Claim.ClaimWithCreatorAndExecutor,
+            newItem: Claim.ClaimWithCreatorAndExecutor
+        ): Boolean {
+            return oldItem == newItem
         }
+
+        override fun areContentsTheSame(
+            oldItem: Claim.ClaimWithCreatorAndExecutor,
+            newItem: Claim.ClaimWithCreatorAndExecutor
+        ): Boolean {
+            return oldItem == newItem
+        }
+    }
 
 }
