7200c32cb554e30ee0aeb8c20eb42b3839667d5b
Stanislav Nanoshkin <nanstas@yandex.ru>
2021-09-14 14:10:42 +0500

    Create ClaimCommentListAdapter.kt

---
+++ app/src/main/java/ru/netology/fmhandroid/adapter/ClaimCommentListAdapter.kt
@@ -0,0 +1,66 @@
+package ru.netology.fmhandroid.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import ru.netology.fmhandroid.databinding.ItemClaimBinding
+import ru.netology.fmhandroid.databinding.ItemCommentBinding
+import ru.netology.fmhandroid.dto.ClaimComment
+import ru.netology.fmhandroid.dto.ClaimWithCreatorAndExecutor
+import ru.netology.fmhandroid.utils.Utils
+
+class ClaimCommentListAdapter() :
+    ListAdapter<ClaimComment, ClaimCommentListAdapter.ClaimCommentViewHolder>(
+        ClaimCommentDiffCallback()
+    ) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ClaimCommentViewHolder {
+        val binding = ItemCommentBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return ClaimCommentViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ClaimCommentViewHolder, position: Int) {
+        getItem(position)?.let {
+            holder.bind(it)
+        }
+    }
+
+    class ClaimCommentViewHolder(
+        private val binding: ItemCommentBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(claimComment: ClaimComment) {
+            binding.apply {
+                commentTextView.text = claimComment.description
+                commentatorNameTextView.text = claimComment.creatorId.toString()
+                commentTimeTextView.text = claimComment.createDate.toString()
+                editLightImageButton.setOnClickListener {
+                    Toast.makeText(it.context, "This work!", Toast.LENGTH_SHORT). show()
+                }
+            }
+        }
+    }
+
+    class ClaimCommentDiffCallback : DiffUtil.ItemCallback<ClaimComment>() {
+        override fun areItemsTheSame(
+            oldItem: ClaimComment,
+            newItem: ClaimComment
+        ): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(
+            oldItem: ClaimComment,
+            newItem: ClaimComment
+        ): Boolean {
+            return oldItem == newItem
+        }
+    }
+}
