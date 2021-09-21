811d42c8a5fde18cf9ea09452d652b2c9846a420
Dmitriy Dolgov <dolgov2007@inbox.ru>
2021-09-21 09:15:53 +0300

    The display of the functionality of open claim fragment, depending on its status, has been improved.

--- app/src/main/java/ru/netology/fmhandroid/model/ClaimCommentModel.kt
+++
@@ -1,9 +0,0 @@
-package ru.netology.fmhandroid.model
-
-import ru.netology.fmhandroid.dto.ClaimComment
-
-data class ClaimCommentModel(
-    val comments: List<ClaimComment> = emptyList(),
-    val loading: Boolean = false,
-    val error: Boolean = false
-)
