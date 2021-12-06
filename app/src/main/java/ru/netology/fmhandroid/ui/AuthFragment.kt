89208efe639f9c33a28868076fec1238b87d39ea
Stanislav Nanoshkin <nanstas@yandex.ru>
2021-12-06 12:01:13 +0300

    добавил AuthFragment.kt, и поправил нейминг в вестке

---
+++ app/src/main/java/ru/netology/fmhandroid/ui/AuthFragment.kt
@@ -0,0 +1,9 @@
+package ru.netology.fmhandroid.ui
+
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+import ru.netology.fmhandroid.R
+
+@AndroidEntryPoint
+class AuthFragment : Fragment(R.layout.fragment_auth){
+}
