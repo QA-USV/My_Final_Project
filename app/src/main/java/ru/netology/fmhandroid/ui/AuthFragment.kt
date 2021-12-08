38cef30926650bb9463f32d0d496048b3109010e
Dmitry Dolgov <dolgov2007@inbox.ru>
2021-12-08 22:23:16 +0300

    изменен метод скрытия и показа system bar

--- app/src/main/java/ru/netology/fmhandroid/ui/AuthFragment.kt
+++ app/src/main/java/ru/netology/fmhandroid/ui/AuthFragment.kt
@@ -1,9 +1,15 @@
 package ru.netology.fmhandroid.ui
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import dagger.hilt.android.AndroidEntryPoint
 import ru.netology.fmhandroid.R
 
 @AndroidEntryPoint
 class AuthFragment : Fragment(R.layout.fragment_auth){
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
