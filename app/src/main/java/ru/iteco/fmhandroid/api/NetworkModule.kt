5fbf91aac26b84bca5bd0136a31a1a9c456a3884
Dolgov Dmitry <dolgov2007@inbox.ru>
2022-02-05 22:22:17 +0300

    убран ssl сертификат и изменено название приложения

--- app/src/main/java/ru/iteco/fmhandroid/api/NetworkModule.kt
+++ app/src/main/java/ru/iteco/fmhandroid/api/NetworkModule.kt
@@ -22,33 +22,6 @@ import javax.inject.Provider
 @InstallIn(SingletonComponent::class)
 @Module
 object NetworkModule {
-    private val certificate: X509Certificate = ("-----BEGIN CERTIFICATE-----\n" +
-            "MIIDmTCCAoGgAwIBAgIUIekvE6KADZePUtq8XQ2DSih+W9QwDQYJKoZIhvcNAQEL\n" +
-            "BQAwfDELMAkGA1UEBhMCWFgxDDAKBgNVBAgMA04vQTEMMAoGA1UEBwwDTi9BMSAw\n" +
-            "HgYDVQQKDBdTZWxmLXNpZ25lZCBjZXJ0aWZpY2F0ZTEvMC0GA1UEAwwmMTMwLjE5\n" +
-            "My40NC45NjogU2VsZi1zaWduZWQgY2VydGlmaWNhdGUwHhcNMjExMjE0MTc0OTM2\n" +
-            "WhcNMjMxMjE0MTc0OTM2WjB8MQswCQYDVQQGEwJYWDEMMAoGA1UECAwDTi9BMQww\n" +
-            "CgYDVQQHDANOL0ExIDAeBgNVBAoMF1NlbGYtc2lnbmVkIGNlcnRpZmljYXRlMS8w\n" +
-            "LQYDVQQDDCYxMzAuMTkzLjQ0Ljk2OiBTZWxmLXNpZ25lZCBjZXJ0aWZpY2F0ZTCC\n" +
-            "ASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBALDv+ZFieSniXLjwmSaFtsfZ\n" +
-            "WIrSx0AgLGA2efz2LXLuvH14o2gekMY0mqekn3SpuaVcvHfU17kx3QLtiB286Bkv\n" +
-            "a3VfISCVL8qZhTolSWDNyyeU9dZqXZ81hsxXrCZNvGkGtgI5vY74PgpbUaLWPnzW\n" +
-            "fXITgEqyTf4OLvORufKWeelZnioDlr7WJGtZhCff2f5Y+WuWtN3UNVVGmCEB1LwB\n" +
-            "7IoyHOdhDUXOjqkFwes1qSmTqCKAnfdGfMdluPte4BrTPCV/dI0Jckdid489brLm\n" +
-            "r5f7dbZmBpR4yoBrT1oq1yb56+jmI+utCmUhuhR8k3IN91t9i6Mn4dEtHFZ+5UEC\n" +
-            "AwEAAaMTMBEwDwYDVR0RBAgwBocEgsEsYDANBgkqhkiG9w0BAQsFAAOCAQEAJqt9\n" +
-            "JBq5JAdlwF1mbFN9FYGiul4OjbklX7YoQyKJ1Fo3c1NgVLpHNHDg6XwWRGS8eakA\n" +
-            "O3EochdghTRvxT2jULzJfEO/g/BfqUHs6wpS0RSAX92ibfoxlUdurjLGKTww+lfx\n" +
-            "5B3vTY/xuJG6uqTBgQCqRjyg7F0iWCFtmCgE4WLFH5rQVWAvM5DRlYDnedL42jMG\n" +
-            "A2AVYVptHELVYe0eMiyLnltITIqin9ti2LEAnpcor8CqYp9wcxQPXmQ7hG3x8rW9\n" +
-            "tnscoka2qxFjBMqcmGm4Q7yl62nJYNXp5lihXlgjwy+BZPo7h8EWqs6kfmMpKjG8\n" +
-            "AS0Wp/MyXT/+rhBblw==\n" +
-            "-----END CERTIFICATE-----").decodeCertificatePem()
-
-    private val handshakeCertificate: HandshakeCertificates = HandshakeCertificates.Builder()
-        .addTrustedCertificate(certificate)
-        .build()
-
     @Provides
     fun loggingInterceptor() = HttpLoggingInterceptor()
         .apply {
@@ -84,10 +57,6 @@ object NetworkModule {
         val authInterceptor = AuthInterceptor(appAuth)
         val refreshAuthenticator = RefreshAuthenticator(authRepositoryProvider, appAuth)
         return OkHttpClient.Builder()
-            .sslSocketFactory(
-                handshakeCertificate.sslSocketFactory(),
-                handshakeCertificate.trustManager
-            )
             .addInterceptor(interceptor)
             .addInterceptor(authInterceptor)
             .authenticator(refreshAuthenticator)
@@ -98,10 +67,6 @@ object NetworkModule {
     @Provides
     fun nonAuthorizedOkhttp(interceptor: HttpLoggingInterceptor): OkHttpClient {
         return OkHttpClient.Builder()
-            .sslSocketFactory(
-                handshakeCertificate.sslSocketFactory(),
-                handshakeCertificate.trustManager
-            )
             .addInterceptor(interceptor)
             .build()
     }
@@ -111,10 +76,6 @@ object NetworkModule {
     fun refreshOkhttp(interceptor: HttpLoggingInterceptor, appAuth: AppAuth): OkHttpClient {
         val refreshInterceptor = RefreshInterceptor(appAuth)
         return OkHttpClient.Builder()
-            .sslSocketFactory(
-                handshakeCertificate.sslSocketFactory(),
-                handshakeCertificate.trustManager
-            )
             .addInterceptor(refreshInterceptor)
             .addInterceptor(interceptor)
             .build()
